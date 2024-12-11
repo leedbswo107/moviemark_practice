9e2707943cfd64431c230460dfd6caae12cfb95c
leedbswo107 <ljyu1@naver.com>
2024-12-11 19:49:09 +0900

    feat: day2

---
+++ src/components/common/Input/NumberInput.tsx
@@ -0,0 +1,23 @@
+import { forwardRef } from "react";
+
+import type { BaseInputProps } from "@/types/input";
+
+import BaseInput from "./BaseInput";
+
+// 현재 이 컴포넌트는 memo를 사용하지 않고 있어요!
+// 이유:
+// 1. BaseInput을 단순히 감싸는 얇은 래퍼(wrapper) 컴포넌트예요
+// 2. 추가적인 로직이 거의 없어서 memo의 이점이 크지 않을 수 있어요
+// 3. 불필요한 최적화를 피하고 코드를 단순하게 유지하는 것이 더 좋을 수 있어요
+
+export type NumberInputProps = Omit<BaseInputProps, "type">;
+
+const NumberInput = forwardRef<HTMLInputElement, NumberInputProps>(
+  (props, ref) => {
+    return <BaseInput type="number" ref={ref} {...props} />;
+  }
+);
+
+NumberInput.displayName = "NumberInput";
+
+export default NumberInput;
